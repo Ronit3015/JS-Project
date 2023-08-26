0d8c207505942ff0420100e93afbaf4e0a446293
Ronit3015 <mauryaron581@gmail.com>
2023-08-26 14:39:59 +0530

    add extra functions

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ let btn = document.querySelector("button");
 
 btn.addEventListener("click" , async () => {
   let word = document.querySelector("input").value;
-
+  
   let defiArr = await getWord(word);
   show(defiArr);
 } );
@@ -12,10 +12,12 @@ function show(defiArr) {
   for (defi of defiArr) {
     let defContainer = document.querySelector("#defContainer");
     defContainer.innerText = "";
-
+    
     let p = document.createElement("p");
+    p.innerText = "";
     p.innerHTML = `<b style=color:red;>Meaning</b> -${defi.meanings[0].definitions[0].definition}
                 `;
+                
     defContainer.appendChild(p);
     p.classList.add("font")
   }
@@ -30,6 +32,16 @@ async function getWord(word) {
     console.log(res.data)
     return res.data;
   } catch(e) {
+    if(massage="Request failed with status code 404"){
+      console.log("ni");
+      let defContainer = document.querySelector("#defContainer");
+    defContainer.innerText = "";
+    
+    let p = document.createElement("p");
+    p.innerHTML = `NO RESULT FOUND FOR THIS WORD.`;
+    defContainer.appendChild(p);
+    p.classList.add("error")
+    }
     console.log("error - " , e)
     return [];
   }
